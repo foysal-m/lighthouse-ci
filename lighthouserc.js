2735269ae7f8f8f0dcd4148362d919720d50a5cd
Foysal Mohammad <foysal@earlblue.com>
2024-01-09 22:21:11 +0000

    feat: lighthouse server setup

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,7 +5,9 @@ module.exports = {
       url: ["http://localhost:3000/"],
     },
     upload: {
-      target: "temporary-public-storage",
+      target: "lhci",
+      serverBaseUrl: "http://localhost:9001",
+      token: "c75e2acf-4533-4eb0-a068-42b0e336883a",
     },
   },
 };
